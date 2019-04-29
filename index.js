596d84ea5001f01e677f730b1efbad9de367d2a6
Simon <simon.fraipont@gmail.com>
2019-04-29 10:45:01 +0200

    add uuid to generate room names

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const uuidv4 = require('uuid/v4');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
